d9859857f2d8668603e709520b151ed4d5730112
Koddeom <koddeom@gmail.com>
2023-05-23 14:32:03 -0300

    tentativa de correcao

--- src/controllers/jsonFileUpload-Controller.js
+++ src/controllers/jsonFileUpload-Controller.js
@@ -25,40 +25,44 @@ exports.getFileJSON = async (req, res, next) => {
 };
 
 exports.postFileJSON = async (req, res, next) => {
-    try {
-      // Obtém o JSON enviado pelo front-end
-      const arquivoJSON = req.body;
-      const nomeArquivo = arquivoJSON.nome;
-      const conteudoBase64 = arquivoJSON.base64;
-  
-      // Decodifica o conteúdo base64 para uma string
-      const conteudoString = Buffer.from(conteudoBase64, 'base64').toString('utf-8');
-  
-      // Aqui você pode manipular a string como desejar
-      const conteudoManipulado = processaConteudo(conteudoString);
-  
-      // Cria o caminho do arquivo temporário
-      const filePath = path.resolve(__dirname, '..', '..', 'public', 'temp', `${nomeArquivo}.txt`);
-  
-      // Grava o conteúdo manipulado no arquivo
-      fs.writeFileSync(filePath, conteudoManipulado, 'utf-8');
-  
-      // Configura o cabeçalho da resposta HTTP para forçar o download do arquivo
-      res.setHeader('Content-Disposition', `attachment; filename="${nomeArquivo}.txt"`);
-      res.setHeader('Content-Type', 'text/plain');
-  
-      // Lê o conteúdo do arquivo e envia como resposta
-      const fileContent = fs.readFileSync(filePath, 'utf-8');
-      res.send(fileContent);
-  
-      // Remove o arquivo temporário após o download ser concluído
+  try {
+    // Obtém o JSON enviado pelo front-end
+    const arquivoJSON = req.body;
+    const nomeArquivo = arquivoJSON.nome;
+    const conteudoBase64 = arquivoJSON.base64;
+
+    // Decodifica o conteúdo base64 para uma string
+    const conteudoString = Buffer.from(conteudoBase64, 'base64').toString('utf-8');
+
+    // Aqui você pode manipular a string como desejar
+    const conteudoManipulado = processaConteudo(conteudoString);
+
+    // Cria o caminho do arquivo temporário
+    const filePath = path.resolve(__dirname, '..', '..', 'public', 'temp', `${nomeArquivo}.txt`);
+
+    // Grava o conteúdo manipulado no arquivo
+    fs.writeFileSync(filePath, conteudoManipulado, 'utf-8');
+
+    // Configura os cabeçalhos da resposta HTTP para forçar o download do arquivo
+    res.setHeader('Content-Disposition', `attachment; filename="${nomeArquivo}.txt"`);
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Length', fs.statSync(filePath).size);
+
+    // Lê o conteúdo do arquivo e envia como resposta
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
+
+    // Remove o arquivo temporário após o download ser concluído
+    fileStream.on('end', () => {
       fs.unlinkSync(filePath);
-  
-    } catch (error) {
-      console.error(utils.endpointError(error));
-      next(error);
-    }
-  };
+    });
+
+  } catch (error) {
+    console.error(utils.endpointError(error));
+    next(error);
+  }
+};
+
   
 exports.getPostFilePage = async (req, res, next) => {
     try { // renderiza a pagina 'html' index.ejs
