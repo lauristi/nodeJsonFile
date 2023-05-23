fcbcb7b03946ea93edeadc6f4381e71a998b3113
Koddeom <koddeom@gmail.com>
2023-05-23 18:13:28 -0300

    code 99%

--- public/javascripts/jsonFileUploadScripts.js
+++ public/javascripts/jsonFileUploadScripts.js
@@ -1,67 +1,118 @@
 
-//-------------------------------------------------------------------
-// Função responsável por converter o arquivo em um objeto JSON
-//-------------------------------------------------------------------
-function converteArquivo(file) {
+//? Função responsável por converter o arquivo em um objeto JSON
+function converteArquivo(originalFile) {
     return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-  
-      // 1-Inicia a leitura do arquivo como um Data URL
-      reader.readAsDataURL(file);
+        const reader = new FileReader();
 
-      // 2-Quando a leitura do arquivo estiver concluída
-      reader.onload = function() {
-        const conteudoArquivo = reader.result.split(',')[1];
+        // 1-Inicia a leitura do arquivo como um Data URL
+        reader.readAsDataURL(originalFile);
+        // 2-Quando a leitura do arquivo estiver concluída
+        reader.onload = function () {
+            const conteudoArquivo = reader.result.split(',')[1];
 
-        const arquivoJSON = {
-          nome: file.name,
-          base64: conteudoArquivo
+            const arquivoJSON = {
+                nome: originalFile.name,
+                base64: conteudoArquivo
+            };
+
+            // 3- Resolve a Promise com o objeto JSON
+            resolve(arquivoJSON);
         };
-  
-        //3- Resolve a Promise com o objeto JSON
-        resolve(arquivoJSON);
-      };
-  
-      // Se ocorrer um erro durante a leitura do arquivo
-      reader.onerror = function() {
-        // Rejeita a Promise com uma mensagem de erro
-        reject(new Error('Erro ao ler o arquivo.'));
-      };
-      
+
+        // Se ocorrer um erro durante a leitura do arquivo
+        reader.onerror = function () {
+            // Rejeita a Promise com uma mensagem de erro
+            reject(new Error('Erro ao ler o arquivo.'));
+        };
+
     });
-  }
-  
-  //-------------------------------------------------------------------
-  // Função responsável por enviar o arquivo para o endpoint usando o Axios
-  //-------------------------------------------------------------------
-  async function enviaArquivo(arquivoJSON) {
+}
+
+//? Função responsável por enviar o arquivo para o endpoint usando o Axios
+async function enviaArquivo(arquivoJSON) {
     try {
-      // Envia a requisição POST para o endpoint '/jsonFile' com o JSON do arquivo
+
+      exibeAjaxLoader(true); 
+     // Envia a requisição POST 
+     //para o endpoint '/jsonFile' com o JSON do arquivo
       const response = await axios.post('/api/jsonFileUpload', arquivoJSON);
-      console.log(response.data);
+      exibeAjaxLoader(false); 
+      
+      //const mode = 'direct'
+      const mode = 'indirect'
+
+      if (mode == 'direct'){
+          //#Download direto
+          downloadFile(response.data, `${arquivoJSON.nome}`);
+
+        }else{
+          //#Link dinamico
+          const downloadLink = document.createElement('a');
+          downloadLink.href = createDownloadURL(response.data, `${arquivoJSON.nome}`);
+          downloadLink.textContent = `Clique para baixar o novo arquivo: ${arquivoJSON.nome}`;
+          document.body.appendChild(downloadLink);
+      }
+
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
-  //-------------------------------------------------------------------
-  // Função principal que é chamada quando o botão de envio é clicado
-//-------------------------------------------------------------------
-  function carregaArquivo() {
+}
+
+//? Função principal que é chamada quando o botão de envio é clicado
+function carregaArquivo() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
-  
-    // Chama a função converteArquivo para converter o arquivo em um objeto JSON
-    converteArquivo(file)
-      .then(enviaArquivo) // Chama a função enviaArquivo para enviar o JSON para o endpoint
-      .catch(error => {
-        console.error(error);
-      });
-  }
 
-//-------------------------------------------------------------------
-//Loader
-//-------------------------------------------------------------------  
+    // Chama a função converteArquivo para converter o arquivo em um objeto JSON
+     converteArquivo(file)
+            .then(enviaArquivo) // Chama  enviarArquivo arquivoJSON vem por callback
+           .catch(error => {console.error(error);
+    });
+}
+
+//? ==========================================================================
+//? Funções de apoio
+//? ==========================================================================
+
+//# Loader
 function exibeAjaxLoader(mostrar) {
     const ajaxLoader = document.getElementById('ajaxLoader');
     ajaxLoader.style.display = mostrar ? 'block' : 'none';
-  }
+}
+
+//# Recria o arquivo atravês do conteudo retornado. Remontando o arquivo txt
+function downloadFile(data, fileName) {
+
+  //01 Cria um objeto Blob com o conteúdo do arquivo
+  const blob = new Blob([data], {type: 'text/plain'});
+
+  //02 Cria uma URL temporária para o Blob
+  const url = window.URL.createObjectURL(blob);
+
+  //03 Cria um elemento <a> para simular um clique em um link de download
+  const a = document.createElement('a');
+
+  //04 Define a URL do arquivo como o valor do atributo href do elemento <a>
+  a.href = url;
+
+  //05 Define o nome do arquivo a ser baixado como o valor do atributo download do elemento <a>
+  a.download = fileName;
+
+  //06 Simula um clique no elemento <a> para iniciar o download do arquivo
+  a.click();
+
+  //07 Libera a URL temporária do Blob para liberar recursos
+  window.URL.revokeObjectURL(url);
+}
+
+//# Cria uma URL de Download dinamica
+function createDownloadURL(data, fileName) {
+  // Cria um objeto Blob com o conteúdo do arquivo
+  const blob = new Blob([data], {type: 'text/plain'});
+
+  // Cria uma URL temporária para o Blob
+  const url = window.URL.createObjectURL(blob);
+
+  // Retorna a URL temporária para ser usada posteriormente
+  return url;
+}
--- src/controllers/jsonFileUpload-Controller.js
+++ src/controllers/jsonFileUpload-Controller.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
+const mime = require('mime');
 
 const app = express();
 app.use(bodyParser.json());
@@ -25,45 +26,51 @@ exports.getFileJSON = async (req, res, next) => {
 };
 
 exports.postFileJSON = async (req, res, next) => {
-  try {
-    // Obtém o JSON enviado pelo front-end
-    const arquivoJSON = req.body;
-    const nomeArquivo = arquivoJSON.nome;
-    const conteudoBase64 = arquivoJSON.base64;
+    try { // Obtém o JSON enviado pelo front-end
+        const arquivoJSON = req.body;
+        const nomeArquivo = arquivoJSON.nome;
+        const conteudoBase64 = arquivoJSON.base64;
 
-    // Decodifica o conteúdo base64 para uma string
-    const conteudoString = Buffer.from(conteudoBase64, 'base64').toString('utf-8');
+        // Decodifica o conteúdo base64 para uma string
+        const conteudoString = Buffer.from(conteudoBase64, 'base64').toString('utf-8');
 
-    // Aqui você pode manipular a string como desejar
-    const conteudoManipulado = processaConteudo(conteudoString);
+        // Aqui você pode manipular a string como desejar
+        const conteudoManipulado = processaConteudo(conteudoString);
 
-    // Cria o caminho do arquivo temporário
-    const filePath = path.resolve(__dirname, '..', '..', 'public', 'temp', `${nomeArquivo}.txt`);
+        // Cria o caminho do arquivo temporário
+        const filePath = path.resolve(__dirname, '..', '..', 'public', 'temp', `${nomeArquivo}.txt`);
 
-    // Grava o conteúdo manipulado no arquivo
-    fs.writeFileSync(filePath, conteudoManipulado, 'utf-8');
+        // Grava o conteúdo manipulado no arquivo
+        fs.writeFileSync(filePath, conteudoManipulado, 'utf-8');
 
-    // Configura os cabeçalhos da resposta HTTP para forçar o download do arquivo
-    res.setHeader('Content-Disposition', `attachment; filename="${nomeArquivo}.txt"`);
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Length', fs.statSync(filePath).size);
+        // Configura os cabeçalhos da resposta HTTP para forçar o download do arquivo
+        
+        var mimetype = mime.getType(filePath);
 
-    // Lê o conteúdo do arquivo e envia como resposta
-    const fileStream = fs.createReadStream(filePath);
-    fileStream.pipe(res);
+        res.setHeader('Content-Disposition', `attachment; filename=${nomeArquivo}.txt`);
+        //res.setHeader('Content-Type', 'application/octet-stream;charset=utf-8');
+        res.setHeader('Content-Length', fs.statSync(filePath).size);
+        res.setHeader('Content-type', mimetype);
 
-    // Remove o arquivo temporário após o download ser concluído
-    fileStream.on('end', () => {
-      fs.unlinkSync(filePath);
-    });
+        // Envia o arquivo como resposta
+        res.download(filePath, function (err) {
+            if (err) {
+                console.log("error");
+                console.log(err);
+            } else {
+                console.log("Sucess");
+            }
 
-  } catch (error) {
-    console.error(utils.endpointError(error));
-    next(error);
-  }
+        });
+
+
+    } catch (error) {
+        console.error(utils.endpointError(error));
+        next(error);
+    }
 };
 
-  
+
 exports.getPostFilePage = async (req, res, next) => {
     try { // renderiza a pagina 'html' index.ejs
 
